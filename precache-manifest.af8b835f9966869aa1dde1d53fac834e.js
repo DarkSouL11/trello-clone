a7a476fcab4b83e9c7df5b938a99d66847fdde99
Sundeep Babbur <sundeep@interviewbit.com>
2020-12-03 00:12:55 +0530

    Updates

---
+++ precache-manifest.af8b835f9966869aa1dde1d53fac834e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da88e857b0c45586b2a3",
+    "url": "/trello-clone/static/js/runtime~main.da88e857.js"
+  },
+  {
+    "revision": "22f471a190cf2bbbb8c7",
+    "url": "/trello-clone/static/js/main.22f471a1.chunk.js"
+  },
+  {
+    "revision": "328bea67f177b66763e2",
+    "url": "/trello-clone/static/js/1.328bea67.chunk.js"
+  },
+  {
+    "revision": "22f471a190cf2bbbb8c7",
+    "url": "/trello-clone/static/css/main.3c9044a7.chunk.css"
+  },
+  {
+    "revision": "1ff56fa7988c267ba18d431816fd57e5",
+    "url": "/trello-clone/index.html"
+  }
+];
